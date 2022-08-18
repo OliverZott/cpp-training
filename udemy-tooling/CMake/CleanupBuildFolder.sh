554df75fb80745bf88864c46c4d600fad1bf4b5d
Oliver Zott <zott_oliver@web.de>
2022-08-18 21:47:12 +0200

    Cleanup for build folder fixed

--- udemy-tooling/CMake/CleanupBuildFolder.sh
+++ udemy-tooling/CMake/CleanupBuildFolder.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
-# executable permission: chmod +x cleanup.sh
+
+# ---------->  executable permission: chmod +x cleanup.sh
+
+result=$(find -type d -name build)      # process substitution
+
+for i in $result ; do
+    if [ "$(ls $i)" ] ; then 
+        echo --------- Delete content of $i ---------
+        rm -v $i/*
+    else
+        echo "Nothing to clean :)"
+
+    fi
+done
+
+
+# ___________
+# - Remarks -
+# -----------
 
 # Find (pre-) compiled files and delete
 # result=$(find . -type f \( -name "*.o" -o -name "main" \))      # process substitution
-result=$(find -type d -name build)      # process substitution
 # echo $result
 
-for i in $result ; do
-    echo --------- Delete contentn of $i ---------
-    #rm -r $i *
-done
+# remove substring: 
+# https://stackoverflow.com/questions/16623835/remove-a-fixed-prefix-suffix-from-a-string-in-bash
+# dir=${i#"."}
+# echo $dir/*
